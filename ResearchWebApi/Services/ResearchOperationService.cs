52e5aa3f0b5538b05e323ff6b301993d26f58ee5
Alice Li <w110056001@mail.nchu.edu.tw>
2023-01-26 11:49:17 +0800

    feat: check bias only when dead cross

--- ResearchWebApi/Services/ResearchOperationService.cs
+++ ResearchWebApi/Services/ResearchOperationService.cs
@@ -245,7 +245,7 @@ namespace ResearchWebApi.Services
 
                     bool testToBuy = firstDay
                             ? buyShortMaVal > buyLongMaVal
-                            : _transTimingService.TimeToBuyCheckingByBias(price, buyShortMaVal) && _transTimingService.TimeToBuy(buyShortMaVal, buyLongMaVal, prevBuyShortMa, prevBuyLongMaVal, hasQty);
+                            : _transTimingService.TimeToBuy(buyShortMaVal, buyLongMaVal, prevBuyShortMa, prevBuyLongMaVal, hasQty);
                     bool testToSell = (_transTimingService.TimeToSellCheckingByBias(price, buyShortMaVal) &&
                                         _transTimingService.TimeToSell(sellShortMa, sellLongMaVal, prevSellShortMaVal, prevSellLongMaVal, hasQty))
                                         || _transTimingService.TimeToSell(lastTrans, ref maxPrice, price, stock.Date, trailingStopPercentage, hasQty);
